ae44c1dda249a2f24e906d8067d5587b2006e628
hareesh <san@gmail.com>
2021-06-29 15:09:26 +0000

    alert

---
+++ main.tf
@@ -0,0 +1,74 @@
+
+resource "google_monitoring_notification_channel" "notification_channel" {
+  count        = "${length(var.notification_email_addresses)}"
+  project      = "${var.project}"
+  enabled      = true
+  display_name = "Send email to ${element(var.notification_email_addresses, count.index)}"
+  type         = "email"
+
+  labels = {
+    email_address = "${element(var.notification_email_addresses, count.index)}"
+  }
+}
+
+
+resource "google_monitoring_alert_policy" "alert_policy" {
+  display_name = var.display_name
+  combiner     = var.alert_combiner
+  #notification_channels = var.notification_email_addresses
+  notification_channels = "${google_monitoring_notification_channel.notification_channel.*.name}"
+  #notification_channels = concat[
+   # google_monitoring_notification_channel.notification_channel]
+  #notification_channels = [
+  #    "${google_monitoring_notification_channel.notification_channel.*.name}"]
+  
+  
+  conditions   {
+    display_name = "Alert on cpu utilization"
+    
+    condition_threshold  {
+      filter          = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+      
+      duration        = var.duration
+      comparison      = var.condition_comparison
+      threshold_value = 0.5
+      trigger {
+        count = var.trigger_count
+      }
+      aggregations {
+        alignment_period   = var.duration
+        per_series_aligner = var.aggregations_aligner
+      }
+    }
+  
+  }
+
+  conditions   {
+    display_name = "Alert for used disk space for filestore"
+        
+    condition_threshold  {
+      filter          = "metric.type=\"file.googleapis.com/nfs/server/used_bytes_percent\" AND resource.type=\"filestore_instance\""
+      
+      duration        = var.duration
+      comparison      = var.condition_comparison
+      threshold_value = 0.8
+      trigger {
+        count = var.trigger_count
+      }
+      aggregations {
+        alignment_period   = var.duration
+        per_series_aligner = var.aggregations_aligner
+      }
+    }
+  }
+}
+/*
+resource "google_monitoring_notification_channel" "notification_channel" {
+  display_name = "Notification Channel"
+  type         = "email"
+
+  labels = {
+    email_address = var.email
+  }
+}
+*/
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "display_name" {
+  type        = string
+  description = "The display name"
+  default = "CPU utilization and filestore"
+}
+
+variable "project" {
+  type        = string
+  description = "The display name"
+  default = "decent-habitat-315907"
+}
+
+variable "alert_combiner" {
+  type        = string
+  description = "Combiner of alert"
+  default = "OR"
+}
+
+variable "duration" {
+  type        = string
+  description = "Duration of the condition"
+  default = "60s"
+}
+
+variable "condition_comparison" {
+  type        = string
+  description = "Comparison such as greater than or lower than of the given condition"
+  default     =  "COMPARISON_GT"
+}
+
+variable "aggregations_aligner" {
+  type        = string
+  description = "Aggregation aligner to be displayed"
+  default  = "ALIGN_NONE"
+}
+
+variable "trigger_count" {
+  type        = number
+  description = "trigger count"
+  default  = 1
+}
+
+
+
+variable "notification_email_addresses" {
+  description = "The email for notifications"
+  type   = list(string)
+  default = ["hareesh5041@gmail.com",
+            "srijitha.s@hcl.com"]
+}
